d87c5020aae6a4d843b8815c9cc1bdc11b82d2d2
Umut Türklay <123954630+umuturklay@users.noreply.github.com>
2024-07-26 17:12:37 +0300

    Frontend with a map but not connected buttons

--- BaşarsoftProje/Program.cs
+++ BaşarsoftProje/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // Register services
 builder.Services.AddScoped<IPointService, PointService>();
